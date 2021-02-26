eef7312991781b5153f3ce5d0d3b051ba4a5aa99
mr-d3falt <79714313+mr-d3falt@users.noreply.github.com>
2021-02-26 21:31:09 +0400

    Add files via upload

---
+++ word_picker.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import sys
+from pyfiglet import Figlet
+custom_fig = Figlet(font='graffiti')
+print(custom_fig.renderText('rockyou.txt'))
+if len(sys.argv) > 1:
+    path = sys.argv[1]
+else:
+    path = input("Please specify path to dictionary:")
+if len(sys.argv) > 2:
+    num = sys.argv[2]
+else:
+    num = input("Please specify number of charachters: ")
+n = int(num)
+with open(r""+path, encoding="latin-1") as file: 
+    allText = file.read() 
+    words = list(map(str, allText.split())) 
+name =("rockyou_"+num+"_ch.txt")    
+new_file= open(name,"w")
+for j in words:
+    if len(j)==n:
+        new_file.write(j+"\n")        
+file.close()
+new_file.close()
+print("\nFinished:")
+print("rockyou_"+num+"_ch.txt was succesifuly created !\n")
+
+
